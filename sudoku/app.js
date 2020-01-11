cf4b88c0ffb84b15ed8d8bcfdeb6b1a2e27b1233
vpoupet <victor.poupet@gmail.com>
2020-01-11 18:08:17 +0100

    - Fixing the sudoku

--- sudoku/app.js
+++ sudoku/app.js
@@ -1,8 +1,9 @@
 let pressedKeys = new Set();
 
-const SudokuGridHook = props => {
+const SudokuGrid = props => {
     const [cellValues, setCellValues] = React.useState(Array(81).fill(undefined));
-    const [cellLocks, setCellLocks] = React.useState(81).fill(false);
+    const [cellLocks, setCellLocks] = React.useState(Array(81).fill(false));
+    const [cellAnnotations, setCellAnnotations] = React.useState(Array(81).fill(Array(9).fill(false)));
     const [worker, setWorker] = React.useState(undefined);
     const cells = React.useRef([]);
 
@@ -12,14 +13,14 @@ const SudokuGridHook = props => {
     const solve = () => {
         if (isIdle()) {
             // start a new computation with a webworker
-            let newWorker = new Worker('./solver.js');
+            let newWorker = new Worker('solver.js');
             newWorker.postMessage({ command: "solve", values: cellValues });
 
             newWorker.onmessage = event => {
                 if (event.data !== undefined) {
                     setCellValues(event.data);
                 }
-                worker.terminate();
+                newWorker.terminate();
                 setWorker(undefined);
             };
             setWorker(newWorker);
@@ -82,7 +83,7 @@ const SudokuGridHook = props => {
         React.createElement(
             'div',
             { className: 'grid', tabIndex: '0' },
-            cellValues.map((v, i) => React.createElement(CellHook, {
+            cellValues.map((v, i) => React.createElement(Cell, {
                 key: i,
                 index: i,
                 value: v,
@@ -123,172 +124,7 @@ const SudokuGridHook = props => {
     );
 };
 
-class SudokuGrid extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cellValues: Array(81).fill(undefined),
-            cellLocks: Array(81).fill(false),
-            worker: undefined
-        };
-        this.cells = [];
-    }
-
-    isIdle() {
-        return this.state.worker === undefined;
-    }
-
-    /**
-     * Mark all cells that have a value as locked
-     */
-    lock() {
-        this.setState({
-            cellLocks: this.state.cellValues.map(v => v !== undefined)
-        });
-    }
-
-    /**
-     * Mark all cells as unlocked
-     */
-    unlock() {
-        this.setState({
-            cellLocks: Array(81).fill(false)
-        });
-    }
-
-    /**
-     * Solve the grid with current values
-     * This method calls a Webworker in the background to run the search algorithm
-     */
-    solve() {
-        if (this.isIdle()) {
-            // start a new computation with a webworker
-            let worker = new Worker('./solver.js');
-            worker.postMessage({
-                command: "solve",
-                values: this.state.cellValues });
-
-            worker.onmessage = event => {
-                if (event.data !== undefined) {
-                    this.setState({
-                        cellValues: event.data
-                    });
-                }
-                this.state.worker.terminate();
-                this.setState({ worker: undefined });
-            };
-            this.setState({ worker: worker });
-        } else {
-            // stop current computation
-            this.state.worker.terminate();
-            this.setState({ worker: undefined });
-        }
-    }
-
-    /**
-     * Solve the grid with current values
-     * This method calls a Webworker in the background to run the search algorithm
-     */
-    generate() {
-        if (this.isIdle()) {
-            // start a new computation with a webworker
-            let worker = new Worker('./solver.js');
-            worker.postMessage({
-                command: "generate" });
-
-            worker.onmessage = event => {
-                if (event.data !== undefined) {
-                    this.setState({
-                        cellValues: event.data,
-                        cellLocks: event.data.map(v => v !== undefined)
-                    });
-                }
-                this.state.worker.terminate();
-                this.setState({ worker: undefined });
-            };
-
-            this.setState({ worker: worker });
-        } else {
-            // stop current computation
-            this.state.worker.terminate();
-            this.setState({ worker: undefined });
-        }
-    }
-
-    /**
-     * Unset the value for all unlocked cells
-     */
-    clear() {
-        if (this.isIdle()) {
-            this.setState({
-                cellValues: this.state.cellValues.map((v, i) => this.state.cellLocks[i] ? v : undefined)
-            });
-        }
-        for (let cell of this.cells) {
-            cell.setState({
-                small: Array(9).fill(false)
-            });
-        }
-    }
-
-    render() {
-        return React.createElement(
-            'div',
-            null,
-            React.createElement(
-                'div',
-                { className: 'grid', tabIndex: '0' },
-                this.state.cellValues.map((v, i) => React.createElement(Cell, {
-                    key: i,
-                    index: i,
-                    value: v,
-                    isLocked: this.state.cellLocks[i],
-                    setValue: v => this.setCellValue(i, v),
-                    ref: input => this.cells[i] = input
-                }))
-            ),
-            React.createElement(
-                'div',
-                { id: 'buttons' },
-                React.createElement(
-                    'button',
-                    { onClick: () => this.generate() },
-                    this.isIdle() ? "Generate" : "Stop"
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: () => this.clear() },
-                    'Clear'
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: () => this.solve() },
-                    this.isIdle() ? "Solve" : "Stop"
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: () => this.lock() },
-                    'Lock'
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: () => this.unlock() },
-                    'Unlock'
-                )
-            )
-        );
-    }
-
-    setCellValue(index, value) {
-        if (!this.state.cellLocks[index]) {
-            let values = [...this.state.cellValues];
-            values[index] = value;
-            this.setState({ cellValues: values });
-        }
-    }
-}
-
-const CellHook = props => {
+const Cell = props => {
     const [small, setSmall] = React.useState(Array(9).fill(false));
 
     let pointerTimer = undefined;
@@ -366,94 +202,6 @@ const CellHook = props => {
     );
 };
 
-class Cell extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            small: Array(9).fill(false)
-        };
-        this.pointerTimer = undefined;
-        this.pointerValue = undefined;
-    }
-
-    render() {
-        let row = ~~(this.props.index / 9);
-        let col = this.props.index % 9;
-        let area = 3 * ~~(row / 3) + ~~(col / 3);
-        let classes = ['cell', `R${row}`, `C${col}`, `A${area}`];
-        if (this.props.isLocked) classes.push("locked");
-        let smallNotes = [];
-        if (this.props.value === undefined) {
-            for (let i = 1; i <= 9; i++) {
-                smallNotes.push(React.createElement(
-                    'div',
-                    {
-                        className: 'small',
-                        onMouseDown: e => this.onPointerDown(e, i),
-                        onMouseLeave: () => this.onPointerLeave(),
-                        onMouseUp: e => this.onPointerUp(e, i),
-                        onPointerDown: e => this.onPointerDown(e, i),
-                        onPointerLeave: () => this.onPointerLeave(),
-                        onPointerUp: e => this.onPointerUp(e, i)
-                        // onTouchStart={e => this.onPointerDown(e, i)}
-                        // onTouchEnd={e => this.onPointerUp(e, i)}
-                        , key: i },
-                    this.state.small[i] ? i : ""
-                ));
-            }
-        }
-        return React.createElement(
-            'div',
-            { className: classes.join(' ') },
-            React.createElement(
-                'div',
-                {
-                    className: 'big',
-                    onTouchStart: () => {
-                        this.props.setValue(undefined);
-                    },
-                    onMouseDown: () => {
-                        this.props.setValue(undefined);
-                    } },
-                this.props.value
-            ),
-            smallNotes
-        );
-    }
-
-    clearPointerTimer() {
-        clearTimeout(this.pointerTimer);
-        this.pointerTimer = undefined;
-    }
-
-    onPointerDown(event, value) {
-        if (!this.props.isLocked) {
-            if (this.pointerTimer !== undefined) {
-                this.clearPointerTimer();
-            }
-            this.pointerTimer = setTimeout(() => {
-                console.log(this.pointerValue);
-                this.props.setValue(this.pointerValue);
-                this.clearPointerTimer();
-            }, 250);
-            this.pointerValue = value;
-        }
-    }
-
-    onPointerUp(event, value) {
-        if (this.pointerTimer !== undefined) {
-            this.clearPointerTimer();
-            let small = Array.from(this.state.small);
-            small[value] = !small[value];
-            this.setState({ small: small });
-        }
-    }
-
-    onPointerLeave(event) {
-        this.clearPointerTimer();
-    }
-}
-
 window.oncontextmenu = function (event) {
     event.preventDefault();
     event.stopPropagation();
@@ -467,6 +215,6 @@ document.addEventListener("keyup", event => {
     pressedKeys.delete(event.key);
 });
 
-ReactDOM.render(React.createElement(SudokuGridHook, null), document.getElementById('app'));
+ReactDOM.render(React.createElement(SudokuGrid, null), document.getElementById('app'));
 
 //# sourceMappingURL=app.js.map
--- sudoku/app.jsx
+++ sudoku/app.jsx
@@ -1,8 +1,12 @@
 let pressedKeys = new Set();
 
-const SudokuGridHook = props => {
+const SudokuGrid = props => {
     const [cellValues, setCellValues] = React.useState(Array(81).fill(undefined));
-    const [cellLocks, setCellLocks] = React.useState(81).fill(false);
+    const [cellLocks, setCellLocks] = React.useState(Array(81).fill(false));
+    const [cellAnnotations, setCellAnnotations] = React.useState(
+        Array(81).fill(
+            Array(9).fill(false)
+        ));
     const [worker, setWorker] = React.useState(undefined);
     const cells = React.useRef([]);
 
@@ -12,14 +16,14 @@ const SudokuGridHook = props => {
     const solve = () => {
         if (isIdle()) {
             // start a new computation with a webworker
-            let newWorker = new Worker('./solver.js');
+            let newWorker = new Worker('solver.js');
             newWorker.postMessage({command: "solve", values: cellValues});
 
             newWorker.onmessage = event => {
                 if (event.data !== undefined) {
                     setCellValues(event.data);
                 }
-                worker.terminate();
+                newWorker.terminate();
                 setWorker(undefined);
             };
             setWorker(newWorker);
@@ -80,7 +84,7 @@ const SudokuGridHook = props => {
         <div>
             <div className="grid" tabIndex="0">
                 { cellValues.map((v, i) =>
-                    <CellHook
+                    <Cell
                         key={i}
                         index={i}
                         value={v}
@@ -101,150 +105,7 @@ const SudokuGridHook = props => {
     );
 };
 
-class SudokuGrid extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cellValues: Array(81).fill(undefined),
-            cellLocks: Array(81).fill(false),
-            worker: undefined,
-        };
-        this.cells = [];
-    }
-
-    isIdle() {
-        return this.state.worker === undefined;
-    }
-
-    /**
-     * Mark all cells that have a value as locked
-     */
-    lock() {
-        this.setState({
-            cellLocks: this.state.cellValues.map(v => v !== undefined),
-        });
-    }
-
-    /**
-     * Mark all cells as unlocked
-     */
-    unlock() {
-        this.setState({
-            cellLocks: Array(81).fill(false),
-        });
-    }
-
-    /**
-     * Solve the grid with current values
-     * This method calls a Webworker in the background to run the search algorithm
-     */
-    solve() {
-        if (this.isIdle()) {
-            // start a new computation with a webworker
-            let worker = new Worker('./solver.js');
-            worker.postMessage({
-                command: "solve",
-                values: this.state.cellValues});
-
-            worker.onmessage = event => {
-                if (event.data !== undefined) {
-                    this.setState({
-                        cellValues: event.data,
-                    });
-                }
-                this.state.worker.terminate();
-                this.setState({ worker: undefined })
-            };
-            this.setState({ worker: worker });
-        } else {
-            // stop current computation
-            this.state.worker.terminate();
-            this.setState({ worker: undefined });
-        }
-    };
-
-    /**
-     * Solve the grid with current values
-     * This method calls a Webworker in the background to run the search algorithm
-     */
-    generate() {
-        if (this.isIdle()) {
-            // start a new computation with a webworker
-            let worker = new Worker('./solver.js');
-            worker.postMessage({
-                command: "generate"});
-
-            worker.onmessage = event => {
-                if (event.data !== undefined) {
-                    this.setState({
-                        cellValues: event.data,
-                        cellLocks: event.data.map(v => v !== undefined),
-                    });
-                }
-                this.state.worker.terminate();
-                this.setState({worker: undefined})
-            };
-
-            this.setState({worker: worker});
-        } else {
-            // stop current computation
-            this.state.worker.terminate();
-            this.setState({ worker: undefined });
-        }
-    };
-
-    /**
-     * Unset the value for all unlocked cells
-     */
-    clear() {
-        if (this.isIdle()) {
-            this.setState({
-                cellValues: this.state.cellValues.map((v, i) => this.state.cellLocks[i] ? v : undefined),
-            });
-        }
-        for (let cell of this.cells) {
-            cell.setState({
-                small: Array(9).fill(false),
-            });
-        }
-    };
-
-    render() {
-        return (
-            <div>
-            <div className="grid" tabIndex="0">
-                { this.state.cellValues.map((v, i) =>
-                    <Cell
-                        key={i}
-                        index={i}
-                        value={v}
-                        isLocked={this.state.cellLocks[i]}
-                        setValue={v => this.setCellValue(i, v)}
-                        ref={(input) => this.cells[i] = input}
-                    />
-                )}
-            </div>
-            <div id="buttons">
-                <button onClick={() => this.generate()}>{this.isIdle() ? "Generate" : "Stop"}</button>
-                <button onClick={() => this.clear()}>Clear</button>
-                <button onClick={() => this.solve()}>{this.isIdle() ? "Solve" : "Stop"}</button>
-                <button onClick={() => this.lock()}>Lock</button>
-                <button onClick={() => this.unlock()}>Unlock</button>
-            </div>
-            </div>
-        );
-    }
-
-    setCellValue(index, value) {
-        if (!this.state.cellLocks[index]) {
-            let values = [...this.state.cellValues];
-            values[index] = value;
-            this.setState({cellValues: values});
-        }
-    }
-}
-
-const CellHook = props => {
+const Cell = props => {
     const [small, setSmall] = React.useState(Array(9).fill(false));
 
     let pointerTimer = undefined;
@@ -315,87 +176,6 @@ const CellHook = props => {
     );
 };
 
-class Cell extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            small: Array(9).fill(false),
-        };
-        this.pointerTimer = undefined;
-        this.pointerValue = undefined;
-    }
-
-    render() {
-        let row = ~~(this.props.index / 9);
-        let col = this.props.index % 9;
-        let area = 3 * ~~(row / 3) + ~~(col / 3);
-        let classes = ['cell', `R${row}`, `C${col}`, `A${area}`];
-        if (this.props.isLocked) classes.push("locked");
-        let smallNotes = [];
-        if (this.props.value === undefined) {
-            for (let i = 1; i <= 9; i++) {
-                smallNotes.push(
-                    <div
-                        className="small"
-                        onMouseDown={e => this.onPointerDown(e, i)}
-                        onMouseLeave={() => this.onPointerLeave()}
-                        onMouseUp={e => this.onPointerUp(e, i)}
-                        onPointerDown={e => this.onPointerDown(e, i)}
-                        onPointerLeave={() => this.onPointerLeave()}
-                        onPointerUp={e => this.onPointerUp(e, i)}
-                        // onTouchStart={e => this.onPointerDown(e, i)}
-                        // onTouchEnd={e => this.onPointerUp(e, i)}
-                        key={i}>
-                        {this.state.small[i] ? i : ""}
-                    </div>);
-            }
-        }
-        return (
-            <div className={classes.join(' ')}>
-                <div
-                    className="big"
-                    onTouchStart={() => {this.props.setValue(undefined)}}
-                    onMouseDown={() => {this.props.setValue(undefined)}}>
-                    { this.props.value }
-                </div>
-                { smallNotes }
-            </div>
-        );
-    }
-
-    clearPointerTimer() {
-        clearTimeout(this.pointerTimer);
-        this.pointerTimer = undefined;
-    }
-
-    onPointerDown(event, value) {
-        if (!this.props.isLocked) {
-            if (this.pointerTimer !== undefined) {
-                this.clearPointerTimer();
-            }
-            this.pointerTimer = setTimeout(() => {
-                console.log(this.pointerValue);
-                this.props.setValue(this.pointerValue);
-                this.clearPointerTimer();
-            }, 250);
-            this.pointerValue = value;
-        }
-    }
-
-    onPointerUp(event, value) {
-        if (this.pointerTimer !== undefined) {
-            this.clearPointerTimer();
-            let small = Array.from(this.state.small);
-            small[value] = !small[value];
-            this.setState({small: small});
-        }
-    }
-
-    onPointerLeave(event) {
-        this.clearPointerTimer();
-    }
-}
-
 window.oncontextmenu = function(event) {
     event.preventDefault();
     event.stopPropagation();
@@ -406,7 +186,7 @@ document.addEventListener("keydown", event => { pressedKeys.add(event.key); });
 document.addEventListener("keyup", event => { pressedKeys.delete(event.key); });
 
 ReactDOM.render(
-    <SudokuGridHook />,
+    <SudokuGrid />,
     document.getElementById('app')
 );
 
